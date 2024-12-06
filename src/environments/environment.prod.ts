85dbfd1ea26181f7755e36bb8ff2fc3112134fab
Seyth382 <nicoelias1016@gmail.com>
2024-12-06 16:06:44 -0300

    key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
+import { initializeApp } from "firebase/app";
+
 export const environment = {
   production: true,
+  apiUrl: "https://uber-nodejs-server-git-d61f89-guillermovillacuratorres-projects.vercel.app/api/",
   firebaseConfig: {
     apiKey: "AIzaSyBlMq0oh1vjxsK9umDfeqtThAa6sS0zSNk",
     authDomain: "vargasyanez-2024.firebaseapp.com",
@@ -9,3 +12,6 @@ export const environment = {
     appId: "1:486413180619:web:b9bbbf218d1405b0c5054e"
   }
 };
+
+
+
